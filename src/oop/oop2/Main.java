fc07e310f6e57c1867be218baba20ce00b751ff4
rwiteshbera <rwiteshbera@gmail.com>
2021-12-29 20:39:52 +0530

    Updated

--- src/oop/oop2/Main.java
+++ src/oop/oop2/Main.java
@@ -1,20 +1,13 @@
 package oop.oop2;
 
-class Calculator {
-    int num1;
-    int num2;
-
-    Calculator (int a, int b) {
-        num1 = a;
-        num2 = b;
-        System.out.println(num1 + num2);
+class Solution {
+    static {
+        System.out.println("Static block invoked");
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Calculator calc = new Calculator(5,8);
-
-        System.out.println(calc);
+        Solution s = new Solution();
     }
 }
